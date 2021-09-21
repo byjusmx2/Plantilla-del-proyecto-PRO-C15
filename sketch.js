078b284712014c2b7f9da7be6080d3e94a9f0321
byjusmx2 <87038490+byjusmx2@users.noreply.github.com>
2021-09-21 12:28:53 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var pathImg,boyImg,cashImg,diamondsImg,jewelryImg,swordImg;
 var treasureCollection = 0;
 var cashG,diamondsG,jewelryG,swordGroup;
 
-//Game States
+//GameState (Estados del juego)
 var PLAY=1;
 var END=0;
 var gameState=1;
@@ -21,13 +21,13 @@ function preload(){
 function setup(){
   
   createCanvas(400,600);
-// Moving background
+// Mover el fondo
 path=createSprite(200,200);
 path.addImage(pathImg);
 path.velocityY = 4;
 
 
-//creating boy running
+//crear sprite de boy (niño) corriendo
 boy = createSprite(70,580,20,20);
 boy.addAnimation("SahilRunning",boyImg);
 boy.scale=0.08;
@@ -49,7 +49,7 @@ function draw() {
   edges= createEdgeSprites();
   boy.collide(edges);
   
-  //code to reset the background
+  //código para reiniciar el fondo
   if(path.y > 400 ){
     path.y = height/2;
   }
